0b753c44502c10e9ff571cba8d7ac729a10d42cf
Gabriel Silva <gafsel@gmail.com>
2016-10-08 19:57:40 -0300

    Corrigindo busca e apresentação de notificações

--- src/main/webapp/www/js/notificacao/notificacao.controller.js
+++ src/main/webapp/www/js/notificacao/notificacao.controller.js
@@ -7,7 +7,13 @@ calvinApp.config(['$stateProvider', function($stateProvider){
                     templateUrl: 'js/notificacao/notificacao.list.html',
                     controller: function(notificacaoService, $scope, $rootScope){
                         $scope.searcher = function(page, callback){
-                            notificacaoService.busca({pagina: page, total: 10}, callback);
+                            notificacaoService.busca({pagina: page, total: 10}, function(notificacoes){
+                                var ns = [];
+                                notificacoes.forEach(function(n){
+                                    ns.push(angular.toJson(n));
+                                });
+                                callback(ns);
+                            });
                             $rootScope.notifications = 0;
                         };
                         
