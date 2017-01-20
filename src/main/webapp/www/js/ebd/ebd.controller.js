3c8db4776b0d619d241838ff09649eb14c23b5ce
Gabriel Silva <gabriel.silva@mirante.net.br>
2017-01-20 21:04:25 -0200

    Acesso a cursos EBD

---
+++ src/main/webapp/www/js/ebd/ebd.controller.js
@@ -0,0 +1,112 @@
+calvinApp.config(['$stateProvider', function($stateProvider){
+    $stateProvider.state('ebd', {
+        parent: 'site',
+        url: '/ebd',
+        views:{
+            'content@':{
+                templateUrl: 'js/ebd/ebd.list.html',
+                controller: function(ebdService, $state, $scope){
+                    $scope.searcher = function(page, callback){
+                        ebdService.busca({pagina:page,total:10}, callback);
+                    };
+
+                    $scope.detalhar = function(ebd){
+                        $state.go('ebd.detalhe', {id: ebd.id});
+                    };
+                }
+            }
+        }
+    }).state('ebd.detalhe', {
+        parent: 'ebd',
+        url: '/:id',
+        views:{
+            'content@':{
+                templateUrl: 'js/ebd/ebd.form.html',
+                controller: function($scope, ebd, $state, ebdService){
+                    $scope.ebd = ebd;
+
+                    $scope.searcherInscricoes = function(page, callback){
+                        ebdService.buscaMinhasInscricoes(ebd.id, {pagina:page,total:10}, callback);
+                    };
+
+                    $scope.$on('$ionicView.enter', function(){
+                        $scope.$broadcast('pagination.search');
+                    });
+
+                    $scope.inscricao = function(){
+                        $state.go('ebd.inscricao', {id: $scope.ebd.id});
+                    };
+                },
+                resolve: {
+                    ebd: ['ebdService', '$stateParams', function(ebdService, $stateParams){
+                        return ebdService.carrega($stateParams.id);
+                    }]
+                }
+            }
+        }
+    }).state('ebd.inscricao', {
+        parent: 'ebd',
+        url: '/:id/inscricao',
+        views:{
+            'content@':{
+                templateUrl: 'js/ebd/inscricao.form.html',
+                controller: function(ebdService, $scope, ebd, $state,
+                                message, $window, $ionicHistory){
+                    $scope.ebd = ebd;
+
+                    $scope.$on('$ionicView.enter', function(){
+                        $scope.clear();
+                    });
+
+                    $scope.clear = function(){
+                        $scope.inscricoes = [];
+                        $scope.addInscricao();
+                    };
+
+                    $scope.addInscricao = function(){
+                        if ($scope.ebd.vagasRestantes > $scope.inscricoes.length){
+                            if ($scope.inscricoes.length){
+                                $scope.inscricoes.push({});
+                            }else{
+                                $scope.inscricoes.push({
+                                    nomeInscrito: $scope.usuario.nome,
+                                    emailInscrito: $scope.usuario.email,
+                                    telefoneInscrito: $scope.usuario.telefones &&
+                                            $scope.usuario.telefones.length ?
+                                    $scope.usuario.telefones[0] : ''
+                                });
+                            }
+                        }
+                    };
+
+                    $scope.removeInscricao = function(inscricao){
+                        $scope.inscricoes.splice($scope.inscricoes.indexOf(inscricao), 1);
+                    };
+
+                    $scope.conclui = function(){
+                        if ($scope.inscricoes.length){
+                            ebdService.inscricao($scope.ebd.id, $scope.inscricoes, function(resposta){
+                                if (resposta.devePagar && resposta.checkoutPagSeguro){
+                                    message({title: 'global.title.200',template: 'mensagens.MSG-042'}, function(){
+                                        $ionicHistory.goBack();
+                                        $window.open('https://pagseguro.uol.com.br/v2/checkout/payment.html?code=' + resposta.checkoutPagSeguro, '_system');
+                                    });
+                                }else{
+                                    message({title: 'global.title.200',template: 'mensagens.MSG-001'});
+                                    $ionicHistory.goBack();
+                                }
+                            });
+                        }
+                    };
+
+                    $scope.clear();
+                },
+                resolve: {
+                    ebd: ['ebdService', '$stateParams', function(ebdService, $stateParams){
+                        return ebdService.carrega($stateParams.id);
+                    }]
+                }
+            }
+        }
+    });
+}]);
