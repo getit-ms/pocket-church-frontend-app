bdcdedff1038987c1191cd70ebbed545089e8efb
Gabriel Silva <gabriel.silva@mirante.net.br>
2017-01-24 17:26:27 -0200

    Ajustes na apresentacao e compartilhamento de videos

--- src/main/webapp/www/js/youtube/youtube.controller.js
+++ src/main/webapp/www/js/youtube/youtube.controller.js
@@ -1,50 +1,56 @@
 calvinApp.config(['$stateProvider', function($stateProvider){
-        $stateProvider.state('youtube', {
-            parent: 'site',
-            url: '/youtube',
-            views:{
-                'content@':{
-                    templateUrl: 'js/youtube/youtube.list.html',
-                    controller: function(youtubeService, $scope, $ionicModal){
-                        $scope.busca = function(){
-                            $scope.futuros = [];
-                            $scope.presentes = [];
-                            $scope.passados = [];
-                            youtubeService.busca(function(videos){
-                                videos.forEach(function(v){
-                                    if (v.aoVivo){
-                                        $scope.presentes.push(v);
-                                    }else if (v.agendamento){
-                                        $scope.futuros.push(v);
-                                    }else{
-                                        $scope.passados.push(v);
-                                    }
-                                });
-                                
-                                $scope.$broadcast('scroll.refreshComplete');
-                            });
-                        };
-                        
-                        $scope.openModal = function(video) {
-                            $scope.video = video;
-                            
-                            $ionicModal.fromTemplateUrl('js/youtube/youtube.modal.html', {
-                                scope: $scope,
-                                animation: 'slide-in-up'
-                            }).then(function(modal) {
-                                $scope.modal = modal;
-                                $scope.modal.show();
-                            });
-                        };
-
-                        $scope.closeModal = function() {
-                            $scope.modal.hide();
-                            $scope.video = undefined;
-                        };
-                        
-                        $scope.busca();
-                    }
+  $stateProvider.state('youtube', {
+    parent: 'site',
+    url: '/youtube',
+    views:{
+      'content@':{
+        templateUrl: 'js/youtube/youtube.list.html',
+        controller: function(youtubeService, $scope, $ionicModal, shareService){
+          $scope.busca = function(){
+            $scope.futuros = [];
+            $scope.presentes = [];
+            $scope.passados = [];
+            youtubeService.busca(function(videos){
+              videos.forEach(function(v){
+                if (v.aoVivo){
+                  $scope.presentes.push(v);
+                }else if (v.agendamento){
+                  $scope.futuros.push(v);
+                }else{
+                  $scope.passados.push(v);
                 }
+              });
+
+              $scope.$broadcast('scroll.refreshComplete');
+            });
+          };
+
+          $scope.openModal = function(video) {
+            $scope.video = video;
+
+            $ionicModal.fromTemplateUrl('js/youtube/youtube.modal.html', {
+              scope: $scope,
+              animation: 'slide-in-up'
+            }).then(function(modal) {
+              $scope.modal = modal;
+              $scope.modal.show();
+            });
+          };
+
+          $scope.share = function(){
+            if ($scope.video){
+              shareService.share({subject:$scope.video.titulo,link:'https://www.youtube.com/watch?v=' + $scope.video.id});
             }
-        });         
-    }]);
+          };
+
+          $scope.closeModal = function() {
+            $scope.modal.hide();
+            $scope.video = undefined;
+          };
+
+          $scope.busca();
+        }
+      }
+    }
+  });
+}]);
