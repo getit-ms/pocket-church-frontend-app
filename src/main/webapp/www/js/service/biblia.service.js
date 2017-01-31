7285a029839ba405147750bc665eddc93c7a3230
Gabriel Silva <gabriel.silva@mirante.net.br>
2017-01-31 11:58:06 -0200

    Ajuste na busca de bíblia

--- src/main/webapp/www/js/service/biblia.service.js
+++ src/main/webapp/www/js/service/biblia.service.js
@@ -2,29 +2,29 @@ calvinApp.service('bibliaService', ['Restangular', 'databaseService', function(R
         this.api = function(){
             return Restangular.all('biblia');
         };
-        
+
         this.sincronizacao = {porcentagem:100};
-        
+
         this.sincroniza = function(){
             var api = this.api;
             var sincronizacao = this.sincronizacao;
-            
+
             var busca = function(pagina, ultimaAtualizacao){
                 var filtro = {
-                    ultimaAtualizacao:formatDate(ultimaAtualizacao),
+                    ultimaAtualizacao:ultimaAtualizacao,
                     pagina:pagina,
-                    total:1
+                    total:3
                 };
-                
+
                 window.localStorage.setItem('filtro_incompleto_biblia', angular.toJson(filtro));
-                
+
                 sincronizacao.porcentagem = 0;
-                
+
                 api().customGET('', filtro).then(function(livros){
                     livros.resultados.forEach(databaseService.mergeLivroBiblia);
-                    
+
                     sincronizacao.porcentagem = 100 * livros.pagina / livros.totalPaginas;
-                    
+
                     if (livros.hasProxima){
                         busca(pagina + 1, ultimaAtualizacao);
                     }else{
@@ -34,32 +34,31 @@ calvinApp.service('bibliaService', ['Restangular', 'databaseService', function(R
                     sincronizacao.porcentagem = 100;
                 });
             };
-            
+
             var filtro = window.localStorage.getItem('filtro_incompleto_biblia');
-            
+
             if (filtro){
                 busca(1, angular.fromJson(filtro).ultimaAtualizacao);
             }else{
                 databaseService.findUltimaAlteracaoLivroBiblia(function(ultimaAtualizacao){
-                    busca(1, ultimaAtualizacao);
+                    busca(1, formatDate(ultimaAtualizacao));
                 });
             }
         };
-        
+
         this.buscaLivros = function(testamento){
             return databaseService.findLivrosBibliaByTestamento(testamento);
         };
-        
+
         this.buscaLivro = function(livro){
             return databaseService.findLivroBiblia(livro);
         };
-        
+
         this.buscaCapitulos = function(livro){
             return databaseService.findCapitulosLivroBiblia(livro);
         };
-        
+
         this.buscaVersiculos = function(livro, capitulo){
             return databaseService.findVersiculosByLivroCapituloBiblia(livro, capitulo);
         };
 }]);
-        
