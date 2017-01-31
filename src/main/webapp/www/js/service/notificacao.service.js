91db76534b6fba190815b1252f9d9e5eef013ab4
Gabriel Silva <gabriel.silva@mirante.net.br>
2017-01-31 16:51:38 -0200

    Alteração na exclusão de notificações

--- src/main/webapp/www/js/service/notificacao.service.js
+++ src/main/webapp/www/js/service/notificacao.service.js
@@ -2,21 +2,20 @@ calvinApp.service('notificacaoService', ['Restangular', function(Restangular){
         this.api = function(){
             return Restangular.one('notificacao');
         };
-        
+
         this.busca = function(filtro, callback){
             this.api().get(filtro).then(callback);
         };
-        
+
         this.count = function(success, error){
             return this.api().one('count').get().then(success, error);
         };
-        
+
         this.clear = function(success){
-            return this.api().one('clear').remove().then(success);
+            this.api().one('clear').remove().then(success);
         };
-        
+
         this.remove = function(id, success){
-            return this.api().one('/' + id).remove().then(success);
+            this.api().one('/' + id).remove().then(success);
         };
 }]);
-        
