acf79992b9fa1e6ef10e378d9299a7bc1c4d185c
Gabriel Silva <gafsel@gmail.com>
2017-04-15 18:16:30 -0300

    FIXes para apresentação de bíblia

--- src/main/webapp/www/js/institucional/institucional.controller.js
+++ src/main/webapp/www/js/institucional/institucional.controller.js
@@ -1,35 +1,36 @@
 calvinApp.config(['$stateProvider', function($stateProvider){
-    $stateProvider.state('institucional', {
-        parent: 'site',
-        url: '/institucional',
-        views:{
-            'content@':{
-                templateUrl: 'js/institucional/institucional.form.html',
-                controller: function(institucionalService, linkService, $scope, cacheService){
-                    angular.extend($scope, linkService);
-                    
-                    cacheService.get({
-                        chave:'institucional',
-                        callback:function(institucional){
-                            $scope.institucional = institucional;
-                            
-                            if (!$scope.institucional.enderecos){
-                                $scope.institucional.enderecos = [];
+        $stateProvider.state('institucional', {
+            parent: 'site',
+            url: '/institucional',
+            views:{
+                'content@':{
+                    templateUrl: 'js/institucional/institucional.form.html',
+                    controller: function(institucionalService, linkService, $scope, cacheService){
+                        angular.extend($scope, linkService);
+                        
+                        cacheService.get({
+                            chave:'institucional',
+                            callback:function(institucional){
+                                $scope.institucional = institucional;
                                 
-                                if ($scope.institucional.endereco){
-                                    $scope.institucional.enderecos.push($scope.institucional.endereco);
+                                if (!$scope.institucional.enderecos){
+                                    $scope.institucional.enderecos = [];
+                                    
+                                    if ($scope.institucional.endereco){
+                                        $scope.institucional.enderecos.push($scope.institucional.endereco);
+                                    }
                                 }
+                                
+                                $scope.institucional.endereco = undefined;
+                            }, 
+                            supplier:function(callback){
+                                institucionalService.carrega(function(institucional){
+                                    callback(institucional);
+                                });
                             }
-                        }, 
-                        supplier:function(callback){
-                            institucionalService.carrega(function(institucional){
-                                callback(institucional);
-                            });
-                        }
-                    });
+                        });
+                    }
                 }
             }
-        }
-    });         
-}]);
-        
+        });         
+    }]);
