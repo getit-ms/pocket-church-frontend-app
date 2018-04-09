4f8ce588c1b6dd2fa9f5b05d6a3acc5dcedce884
Gabriel Silva <gafsel@gmail.com>
2018-04-08 23:06:31 -0300

    Ajustes para build iOS pelo fastlane

--- src/main/webapp/fastlane/configure_entitlements.rb
+++ src/main/webapp/fastlane/configure_entitlements.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 require 'xcodeproj'
 
-proj = Xcodeproj::Project.open("../${nomeAplicativo.igreja};xcodeproj")
+proj = Xcodeproj::Project.open("../${nomeAplicativo.igreja}.xcodeproj")
 
 targetid = proj.targets.first.uuid
-proj.root_object.attributes['TargetAttributes'] = {targetid => {"SystemCapabilities" => {"com.apple.Push" => {"enable" => 1}}}}
+proj.root_object.attributes['TargetAttributes'] = {targetid => {"SystemCapabilities" => {"com.apple.Push" => {"enabled" => 1}}}}
 
 proj.build_configurations.each do |config|
-  config.build_settings.store("CODE_SIGN_ENTITLEMENTS", "${nomeAplicativo.igreja}/pocket-church.entitlements")
+    config.build_settings.store("CODE_SIGN_ENTITLEMENTS", "${nomeAplicativo.igreja}/pocket-church.entitlements")
 end
 
 proj.recreate_user_schemes
