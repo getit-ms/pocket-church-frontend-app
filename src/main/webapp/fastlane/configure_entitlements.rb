c18b91079cb6594c2d4bb558d7412f2dfa28454f
Gabriel Silva <gafsel@gmail.com>
2018-04-09 10:33:53 -0300

    FIX para assinatura de produçao

--- src/main/webapp/fastlane/configure_entitlements.rb
+++ src/main/webapp/fastlane/configure_entitlements.rb
@@ -6,9 +6,5 @@ proj = Xcodeproj::Project.open("../${nomeAplicativo.igreja}.xcodeproj")
 targetid = proj.targets.first.uuid
 proj.root_object.attributes['TargetAttributes'] = {targetid => {"SystemCapabilities" => {"com.apple.Push" => {"enabled" => 1}}}}
 
-proj.build_configurations.each do |config|
-    config.build_settings.store("CODE_SIGN_ENTITLEMENTS", "${nomeAplicativo.igreja}/pocket-church.entitlements")
-end
-
 proj.recreate_user_schemes
 proj.save
