149556c07bbc068bb2176b222b7b3afde7c15741
Gabriel Silva <gafsel@gmail.com>
2018-05-18 07:32:07 -0300

    Ajuste para android

--- src/main/webapp/www/js/usuario/usuario.controller.js
+++ src/main/webapp/www/js/usuario/usuario.controller.js
@@ -65,23 +65,23 @@ calvinApp.config(['$stateProvider', function($stateProvider){
 
                             }, function(error) {
                               console.error(error);
-                              message({title:'global.title.500',template:'mensagens.MSG-052'});
+                              message({title:'global.title.500',template:error.toString()});
                               loadingService.hide();
                             });
 
                           }, function(error) {
                             console.error(error);
-                            message({title:'global.title.500',template:'mensagens.MSG-052'});
+                            message({title:'global.title.500',template:error.toString()});
                             loadingService.hide();
                           });
                         }, function(error) {
                           console.error(error);
-                          message({title:'global.title.500',template:'mensagens.MSG-052'});
+                          message({title:'global.title.500',template:error.toString()});
                           loadingService.hide();
                         });
                     }, function (error) {
                       console.error(error);
-                      message({title:'global.title.500',template:'mensagens.MSG-052'});
+                      message({title:'global.title.500',template:error.toString()});
                       loadingService.hide();
                     }, imageUri, {
                       targetWidth: 500,
