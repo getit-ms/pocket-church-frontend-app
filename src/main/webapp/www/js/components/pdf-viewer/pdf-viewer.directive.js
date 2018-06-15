c4f576b9b01d94ba0ae95b6a50e456a505099f66
Gabriel Silva <gafsel@gmail.com>
2018-06-15 03:38:36 -0300

    Substituindo motor de slide

--- src/main/webapp/www/js/components/pdf-viewer/pdf-viewer.directive.js
+++ src/main/webapp/www/js/components/pdf-viewer/pdf-viewer.directive.js
@@ -12,24 +12,16 @@ calvinApp.directive('pdfViewer', function(){
           $scope.load = function() {
             pdfService.get($scope.arquivo.id, function(pdf) {
               $scope.pdf = pdf;
-              $scope.pages = [];
 
-              var buscaPagina = function(i) {
-                if (i<=pdf.numPages) {
-                  pdf.getPage(i).then(function(page) {
-                    $scope.pages.push(page);
+              initSlide();
 
-                    buscaPagina(i + 1);
-                  }, function(err) {
-                    console.error(err);
-                  });
-                } else {
-                  $scope.$apply();
-                }
-              };
+              $scope.$apply();
+            });
+          };
 
-              buscaPagina(1);
-
+          $scope.supplier = function(nr, callback) {
+            $scope.pdf.getPage(Number(nr)).then(function(page) {
+              callback({page: page});
               $scope.$apply();
             });
           };
@@ -54,6 +46,65 @@ calvinApp.directive('pdfViewer', function(){
               $ionicSlideBoxDelegate.enableSlide(false);
             }
           };
+
+          function initSlide() {
+            var makeSlide = function ( nr ) {
+              var dados = {nr : nr};
+              if ($scope.showSlide(dados)) {
+                $scope.supplier(nr, function(d0){
+                  angular.extend(dados, d0);
+                  $scope.$apply();
+                });
+              }
+              return dados;
+            };
+
+            $scope.slideShow = {first: 1, last: $scope.pdf.numPages};
+
+            $scope.showSlide = function (slide) {
+              return (slide.nr >= $scope.slideShow.first && slide.nr <= $scope.slideShow.last);
+            };
+
+            if (!$scope.initialSlide) { // initial
+              $scope.initialSlide = 1;
+            } else {
+              $scope.initialSlide = Number($scope.initialSlide);
+            }
+
+            $scope.slides = [
+              makeSlide($scope.initialSlide),
+              makeSlide($scope.initialSlide + 1),
+              makeSlide($scope.initialSlide - 1)
+            ];
+
+            console.log('Iniciado com nrs ' + $scope.slides[0].nr  + ', ' + $scope.slides[1].nr + ', ' + $scope.slides[2].nr);
+
+            $scope.slideChanged = function (i) {
+              if (isNaN(i)) return;
+
+              var
+                previous_index = ($scope.slides.length + i - 1) % $scope.slides.length,
+                direction = $scope.slides[i].nr > $scope.slides[previous_index].nr  ? 1 : -1;
+
+              console.log('Carregando idx ' + i + ' nr ' + $scope.slides[i].nr);
+
+              var nrProx = $scope.slides[i].nr + direction;
+              var idxProx = ($scope.slides.length + i + direction) % $scope.slides.length;
+
+              console.log('Preparando idx ' + idxProx + ' nr ' + nrProx);
+
+              if ($scope.slides[idxProx].nr != nrProx) {
+                $scope.slides[idxProx] = makeSlide(nrProx);
+              }
+
+              if ($scope.slides[i].nr < $scope.slideShow.first || $scope.slides[i].nr > $scope.slideShow.last) {
+                console.log('Redirecionando para idx ' + i - direction);
+
+                $ionicSlideBoxDelegate.$getByHandle('slideshow-slidebox').slide(i - direction);
+              }
+
+            };
+          }
 
         }]
     };
