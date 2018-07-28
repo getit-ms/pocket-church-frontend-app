8ce6d64decaaa11ec0e74e559717d1f1991e959b
Gabriel Silva <gafsel@gmail.com>
2018-07-27 22:03:58 -0300

    Ajustes diversos

--- src/main/webapp/www/js/components/gallery-viewer/gallery-viewer.directive.js
+++ src/main/webapp/www/js/components/gallery-viewer/gallery-viewer.directive.js
@@ -34,137 +34,132 @@ calvinApp.directive('galleryViewer', function(){
         }
       };
 
-      function initSlide() {
-
-        var makeSlide = function ( nr ) {
-          var dados = {nr : nr};
-          if ($scope.showSlide(dados)) {
-            $scope.supplier(nr, function(d0){
-              angular.extend(dados, d0);
-              $scope.$apply();
-            });
-          }
-          return dados;
-        };
-
-        $scope.slideShow = {first: 1, last: $scope.total};
-
-        $scope.showSlide = function (slide) {
-          return (slide.nr >= $scope.slideShow.first && slide.nr <= $scope.slideShow.last);
-        };
-
-        if (!$scope.status) {
-          $scope.status = {pagina:1, total:$scope.total};
-        } else {
-          $scope.status.total = $scope.total;
-
-          if (!$scope.status.pagina) { // initial
-            $scope.status.pagina = 1;
-          } else {
-            $scope.status.pagina = Number($scope.status.pagina);
-          }
+      var makeSlide = function ( nr ) {
+        var dados = {nr : nr};
+        if ($scope.showSlide(dados)) {
+          $scope.supplier(nr, function(d0){
+            angular.extend(dados, d0);
+            $scope.$apply();
+          });
         }
+        return dados;
+      };
 
-        $scope.slides = [
-          makeSlide($scope.status.pagina),
-          makeSlide($scope.status.pagina + 1),
-          makeSlide($scope.status.pagina + 2),
-          makeSlide($scope.status.pagina - 2),
-          makeSlide($scope.status.pagina - 1)
-        ];
+      $scope.slideShow = {first: 1, last: $scope.total};
 
-        function cachePaginas() {
-          var
-            i = ((500000 + $scope.slider.activeIndex - 1) % $scope.slides.length),
-            previous_index = (500000 + i - 2) % $scope.slides.length,
-            direction = $scope.slides[i].nr > $scope.slides[previous_index].nr  ? 2 : -2;
+      $scope.showSlide = function (slide) {
+        return (slide.nr >= $scope.slideShow.first && slide.nr <= $scope.slideShow.last);
+      };
 
-          if (i == $scope.lastIndex) return;
+      if (!$scope.status) {
+        $scope.status = {pagina:1, total:$scope.total};
+      } else {
+        $scope.status.total = $scope.total;
 
-          $scope.lastIndex = i;
-
-          $scope.status.pagina = $scope.slides[i].nr;
-
-          var nrProx = $scope.slides[i].nr + direction;
-          var idxProx = ($scope.slides.length + i + direction) % $scope.slides.length;
-
-          if ($scope.slides[idxProx].nr != nrProx) {
-            $scope.slides[idxProx] = makeSlide(nrProx);
-          }
-
-          lockPadroes();
-        };
-
-        $scope.clearTap = function() {
-          clearTimeout($scope.tap);
-          $scope.tap = undefined;
-        };
-
-        $scope.actionClick = function() {
-
-          if (!$scope.tap) {
-            $scope.tap = setTimeout(function() {
-              $scope.clearTap();
-
-              if ($scope.status.click) {
-                $scope.status.click();
-              }
-
-              $scope.$apply();
-            }, 300);
-          } else {
-            $scope.clearTap();
-
-            var index = ($scope.slider.activeIndex - 1);
-
-            var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + index).getScrollPosition().zoom;
-            if (zoomFactor == 1) {
-              $ionicScrollDelegate.$getByHandle('scrollHandle' + index).zoomTo(2, true);
-            } else {
-              $ionicScrollDelegate.$getByHandle('scrollHandle' + index).zoomTo(1, true);
-            }
-
-            setTimeout(function() {
-              $scope.$apply();
-            }, 100);
-          }
-        };
-
-        $scope.status.hasAnterior = function() {
-          return $scope.slider && !$scope.slider.params.onlyExternal &&
-            $scope.slides[(500000 + $scope.slider.activeIndex - 1) % $scope.slides.length].nr > $scope.slideShow.first;
-        };
-
-        $scope.status.hasProximo = function() {
-          return $scope.slider && !$scope.slider.params.onlyExternal &&
-            $scope.slides[(500000 + $scope.slider.activeIndex - 1) % $scope.slides.length].nr < $scope.slideShow.last;
-        };
-
-        $scope.$on('$ionicSlides.sliderInitialized', function(event, data) {
-          $scope.slider = data.slider;
-
-          lockPadroes();
-
-          $scope.status.proximo = function() {
-            if ($scope.status.hasProximo()) {
-              $scope.slider.slideNext();
-              cachePaginas();
-            }
-          };
-
-          $scope.status.anterior = function() {
-            if ($scope.status.hasAnterior()) {
-              $scope.slider.slidePrev();
-              cachePaginas();
-            }
-          };
-
-          $scope.$watch('slider.activeIndex', cachePaginas);
-
-        });
+        if (!$scope.status.pagina) { // initial
+          $scope.status.pagina = 1;
+        } else {
+          $scope.status.pagina = Number($scope.status.pagina);
+        }
       }
 
-      initSlide();
+      $scope.slides = [
+        makeSlide($scope.status.pagina),
+        makeSlide($scope.status.pagina + 1),
+        makeSlide($scope.status.pagina + 2),
+        makeSlide($scope.status.pagina - 2),
+        makeSlide($scope.status.pagina - 1)
+      ];
+
+      function cachePaginas() {
+        var
+          i = ((500000 + $scope.slider.activeIndex - 1) % $scope.slides.length),
+          previous_index = (500000 + i - 2) % $scope.slides.length,
+          direction = $scope.slides[i].nr > $scope.slides[previous_index].nr  ? 2 : -2;
+
+        if (i == $scope.lastIndex) return;
+
+        $scope.lastIndex = i;
+
+        $scope.status.pagina = $scope.slides[i].nr;
+
+        var nrProx = $scope.slides[i].nr + direction;
+        var idxProx = ($scope.slides.length + i + direction) % $scope.slides.length;
+
+        if ($scope.slides[idxProx].nr != nrProx) {
+          $scope.slides[idxProx] = makeSlide(nrProx);
+        }
+
+        lockPadroes();
+      };
+
+      $scope.clearTap = function() {
+        clearTimeout($scope.tap);
+        $scope.tap = undefined;
+      };
+
+      $scope.actionClick = function() {
+
+        if (!$scope.tap) {
+          $scope.tap = setTimeout(function() {
+            $scope.clearTap();
+
+            if ($scope.status.click) {
+              $scope.status.click();
+            }
+
+            $scope.$apply();
+          }, 300);
+        } else {
+          $scope.clearTap();
+
+          var index = ($scope.slider.activeIndex - 1);
+
+          var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + index).getScrollPosition().zoom;
+          if (zoomFactor == 1) {
+            $ionicScrollDelegate.$getByHandle('scrollHandle' + index).zoomTo(2, true);
+          } else {
+            $ionicScrollDelegate.$getByHandle('scrollHandle' + index).zoomTo(1, true);
+          }
+
+          setTimeout(function() {
+            $scope.$apply();
+          }, 100);
+        }
+      };
+
+      $scope.status.hasAnterior = function() {
+        return $scope.slider && !$scope.slider.params.onlyExternal &&
+          $scope.slides[(500000 + $scope.slider.activeIndex - 1) % $scope.slides.length].nr > $scope.slideShow.first;
+      };
+
+      $scope.status.hasProximo = function() {
+        return $scope.slider && !$scope.slider.params.onlyExternal &&
+          $scope.slides[(500000 + $scope.slider.activeIndex - 1) % $scope.slides.length].nr < $scope.slideShow.last;
+      };
+
+      $scope.$on('$ionicSlides.sliderInitialized', function(event, data) {
+        $scope.slider = data.slider;
+
+        lockPadroes();
+
+        $scope.status.proximo = function() {
+          if ($scope.status.hasProximo()) {
+            $scope.slider.slideNext();
+            cachePaginas();
+          }
+        };
+
+        $scope.status.anterior = function() {
+          if ($scope.status.hasAnterior()) {
+            $scope.slider.slidePrev();
+            cachePaginas();
+          }
+        };
+
+        $scope.$watch('slider.activeIndex', cachePaginas);
+
+      });
     }]
   };
 });
