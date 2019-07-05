2bd92cb0bf5de2ab8773fe12aa3860cf78eb2b77
Gabriel Silva <gafsel@gmail.com>
2019-07-05 15:50:06 -0300

    Ajusts para ícone no player

--- src/main/webapp/hooks/before_build/020_copy_resources.js
+++ src/main/webapp/hooks/before_build/020_copy_resources.js
@@ -9,10 +9,16 @@ var resources = [
     {
       from: 'resources/native/android/*',
       to: 'platforms/android/app/src/main/res/'
+    },
+    {
+      from: 'resources/native/android/push.png',
+      to: 'platforms/android/app/src/main/res/mipmap/icon.png'
     }
 ];
 
 function copyAndroidResources() {
+    exec('mkdir platforms/android/app/src/main/res/mipmap');
+
     resources.forEach(function(resource){
         exec('cp -Rf ' + resource.from + ' ' + resource.to);
     });
