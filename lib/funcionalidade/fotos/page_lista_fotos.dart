1197405e27ab9deea0dae38c612a05428a078518
Gabriel Silva <gafsel@gmail.com>
2019-09-02 00:41:29 -0300

    FIX busca de fotos

--- lib/funcionalidade/fotos/page_lista_fotos.dart
+++ lib/funcionalidade/fotos/page_lista_fotos.dart
@@ -12,134 +12,68 @@ class PageListaFotos extends StatelessWidget {
       withAppBar: false,
       body: Container(
         color: Colors.white,
-        child: FutureBuilder<Pagina<Foto>>(
-            future: fotoApi.consulta(
+        child: SliverInfiniteList<Foto>(
+          provider: (pagina, tamanho) async {
+            return await fotoApi.consulta(
               galeria: galeria.id,
-              pagina: 1,
-              tamanhoPagina: galeria.quantidadeFotos ?? 500,
-            ),
-            builder: (context, snapshot) {
-              return CustomScrollView(
-                slivers: <Widget>[
-                  _header(),
-                  _descricao(),
-                  _fotos(context, snapshot),
-                ],
+              pagina: pagina,
+              tamanhoPagina: tamanho,
+            );
+          },
+          builder: (context, itens, index) {
+            Foto foto = itens[index];
+
+            if (index % 3 == 0) {
+              return Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 10,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: _linkFoto(
+                        context: context,
+                        foto: foto,
+                        onPressed: onClick(context, itens, index),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: index + 1 < itens.length
+                          ? _linkFoto(
+                              context: context,
+                              foto: itens[index + 1],
+                              onPressed: onClick(context, itens, index + 1),
+                            )
+                          : Container(),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                        child: index + 2 < itens.length
+                            ? _linkFoto(
+                                context: context,
+                                foto: itens[index + 2],
+                                onPressed: onClick(context, itens, index + 2),
+                              )
+                            : Container())
+                  ],
+                ),
               );
-            }),
+            }
+
+            return Container();
+          },
+          preSlivers: <Widget>[
+            _header(),
+            _descricao(),
+          ],
+        ),
       ),
     );
-  }
-
-  _fotos(BuildContext context, AsyncSnapshot<Pagina<Foto>> snapshot) {
-    if (snapshot.connectionState == ConnectionState.done) {
-      if (snapshot.hasData && snapshot.data.totalResultados > 0) {
-        return SliverGrid(
-          delegate: SliverChildListDelegate(
-            snapshot.data.resultados
-                .map((foto) => _linkFoto(
-                    context: context,
-                    foto: foto,
-                    onPressed: () {
-                      showCupertinoModalPopup(
-                        context: context,
-                        builder: (ctx) => PhotoViewPage(
-                          onDownload: Platform.isAndroid
-                              ? (index) {
-                                  Foto foto = snapshot.data.resultados[index];
-
-                                  arquivoService
-                                      .download(
-                                    "https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_b.jpg",
-                                    "${foto.id}_${foto.secret}_b.jpg",
-                                  )
-                                      .then((file) {
-                                    MessageHandler.success(
-                                      Scaffold.of(context),
-                                      IntlText(
-                                        "mensagens.MSG-056",
-                                        args: {
-                                          'filename': file.substring(
-                                              file.lastIndexOf("/") + 1),
-                                        },
-                                      ),
-                                    );
-                                  }).catchError((err) {
-                                    MessageHandler.error(
-                                      Scaffold.of(context),
-                                      const IntlText("mensagens.MSG-055"),
-                                    );
-                                  });
-                                }
-                              : null,
-                          onShare: (index) {
-                            Foto foto = snapshot.data.resultados[index];
-
-                            arquivoService
-                                .downloadTemp(
-                              "https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_b.jpg",
-                            )
-                                .then((file) async {
-                              final ByteData bytes =
-                                  await services.rootBundle.load(file);
-                              Share.file(
-                                galeria.nome,
-                                "${foto.id}_${foto.secret}_b.jpg",
-                                bytes.buffer.asUint8List(),
-                                'image/jpeg',
-                              );
-                            }).catchError((err) {
-                              MessageHandler.error(
-                                Scaffold.of(context),
-                                const IntlText("mensagens.MSG-055"),
-                              );
-                            });
-                          },
-                          pageController: PageController(
-                              initialPage:
-                                  snapshot.data.resultados.indexOf(foto)),
-                          images: snapshot.data.resultados
-                              .map((foto) => ImageView(
-                                  image: NetworkImage(
-                                      "https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_b.jpg"),
-                                  heroTag: 'foto_' + foto.id))
-                              .toList(),
-                          title: Text(galeria.nome),
-                        ),
-                      );
-                    }))
-                .toList(),
-          ),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 5,
-          ),
-        );
-      }
-
-      return SliverList(
-          delegate: SliverChildListDelegate(
-        [
-          const Padding(
-            padding: EdgeInsets.all(20),
-            child: Center(
-              child: IntlText("global.nenhum_registro_encontrado"),
-            ),
-          )
-        ],
-      ));
-    }
-
-    return SliverList(
-        delegate: SliverChildListDelegate([
-      Padding(
-        padding: const EdgeInsets.all(20),
-        child: Center(
-          child: const CircularProgressIndicator(),
-        ),
-      )
-    ]));
   }
 
   SliverAppBar _header() {
@@ -199,19 +133,88 @@ class PageListaFotos extends StatelessWidget {
   }
 
   Widget _linkFoto({BuildContext context, Foto foto, VoidCallback onPressed}) {
-    return RawMaterialButton(
-      onPressed: onPressed,
-      child: Hero(
-        tag: 'foto_' + foto.id,
-        child: FadeInImage(
-          width: double.infinity,
-          height: double.infinity,
-          placeholder: MemoryImage(kTransparentImage),
-          image: NetworkImage(
-              "https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_n.jpg"),
-          fit: BoxFit.cover,
+    return SizedBox(
+      height: MediaQuery.of(context).size.width / 3 - 20,
+      width: double.infinity,
+      child: RawMaterialButton(
+        onPressed: onPressed,
+        child: Hero(
+          tag: 'foto_' + foto.id,
+          child: FadeInImage(
+            width: double.infinity,
+            height: double.infinity,
+            placeholder: MemoryImage(kTransparentImage),
+            image: NetworkImage(
+                "https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_n.jpg"),
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
+  }
+
+  onClick(BuildContext context, List<Foto> itens, int index) {
+    Foto foto = itens[index];
+
+    return () {
+      showCupertinoModalPopup(
+        context: context,
+        builder: (ctx) => PhotoViewPage(
+          onDownload: Platform.isAndroid
+              ? (index) {
+                  arquivoService
+                      .download(
+                    "https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_b.jpg",
+                    "${foto.id}_${foto.secret}_b.jpg",
+                  )
+                      .then((file) {
+                    MessageHandler.success(
+                      Scaffold.of(context),
+                      IntlText(
+                        "mensagens.MSG-056",
+                        args: {
+                          'filename': file.substring(file.lastIndexOf("/") + 1),
+                        },
+                      ),
+                    );
+                  }).catchError((err) {
+                    MessageHandler.error(
+                      Scaffold.of(context),
+                      const IntlText("mensagens.MSG-055"),
+                    );
+                  });
+                }
+              : null,
+          onShare: (index) {
+            arquivoService
+                .downloadTemp(
+              "https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_b.jpg",
+            )
+                .then((file) async {
+              final ByteData bytes = await services.rootBundle.load(file);
+              Share.file(
+                galeria.nome,
+                "${foto.id}_${foto.secret}_b.jpg",
+                bytes.buffer.asUint8List(),
+                'image/jpeg',
+              );
+            }).catchError((err) {
+              MessageHandler.error(
+                Scaffold.of(context),
+                const IntlText("mensagens.MSG-055"),
+              );
+            });
+          },
+          pageController: PageController(initialPage: index),
+          images: itens
+              .map((foto) => ImageView(
+                  image: NetworkImage(
+                      "https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_b.jpg"),
+                  heroTag: 'foto_' + foto.id))
+              .toList(),
+          title: Text(galeria.nome),
+        ),
+      );
+    };
   }
 }
