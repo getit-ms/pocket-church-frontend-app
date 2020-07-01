458e153bf73ffca185558b0f72fab8b3948e100c
Gabriel Silva <gafsel@gmail.com>
2020-07-01 16:27:47 -0300

    Ajustes para build iOS

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -8,4 +8,6 @@ BUILD_NUMBER=$(printf "%d%02d%02d" $d1 $d2 $d3) || exit 1
 
 echo "Construindo iOS $BUILD_NUMBER"
 
-flutter build ios --build-number=$BUILD_NUMBER --build-name=$1 || exit
+rm -f ios/Podfile*
+
+flutter build ios --release --build-number=$BUILD_NUMBER --build-name=$1 || exit
